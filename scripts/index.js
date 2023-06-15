b2077e0e1a5dcd63e027553ce718d78ddcc3851a
Elena Kovacheska <kovacheskaelena@gmail.com>
2023-06-15 21:32:02 +0200

    Implemented light and dark mode functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,20 @@
 import renderPosts from "./loadMore.js";
 
-// Initial rendering
-renderPosts();
+document.addEventListener("DOMContentLoaded", () => {
+	const switchElement = document.getElementById("switch");
+	let isDarkMode = false;
+
+	function toggleDarkMode() {
+		isDarkMode = !isDarkMode;
+		if (isDarkMode) {
+			document.body.classList.add("dark-mode");
+		} else {
+			document.body.classList.remove("dark-mode");
+		}
+	}
+
+	switchElement.addEventListener("click", toggleDarkMode);
+
+	// Initial rendering
+	renderPosts();
+});
